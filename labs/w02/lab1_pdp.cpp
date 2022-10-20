de8422cd875b56b7e7ce42f4b0a5541e8bbf05ea
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-10-20 22:00:45 +0300

    add run_transactions_worker threads problem: sometimes account 0 ends with -1 balance

--- labs/w02/lab1_pdp.cpp
+++ labs/w02/lab1_pdp.cpp
@@ -8,6 +8,7 @@
 #include <thread>
 #include <ctime>
 #include <list>
+#include <vector>
 
 using std::unordered_map;
 using std::string;
@@ -15,6 +16,7 @@ using std::ifstream;
 using std::cout;
 using std::mutex;
 using std::list;
+using std::vector;
 //endregion }}}
 
 //region typedef {{{
@@ -34,10 +36,13 @@ typedef struct {
 // sau n tranzactii sau intr-un for loop
 
 //region globals {{{
+#define THREADS 30
 #define TRANSACTIONS_PER_THREAD 1
 unordered_map<int, Account> _accounts;
 int id_base;
+int transaction_id_base;
 mutex accounts_mutex;
+mutex transaction_id_mutex;
 list<Transaction> transactions;
 //endregion }}}
 
@@ -70,6 +75,10 @@ int rand(int upperExclusiveBound) {
 int rand(int lowerBoundInclusive, int upperExclusiveBound) {
     return rand(upperExclusiveBound) + lowerBoundInclusive;
 }
+
+bool randBool() {
+    return rand(2) == 1;
+}
 //endregion }}}
 
 //region account workers {{{
@@ -97,8 +106,40 @@ void read_last_account_worker() {
 
 void run_transactions_worker() {
     for(int i = 0; i < TRANSACTIONS_PER_THREAD; i++) {
+        Transaction transaction;
+        transaction.source_id = rand(id_base);
+        // generate 2nd random id, POSSIBLY UNSAFE
+        transaction.destination_id = randBool() && transaction.source_id != 0 ?
+                rand(transaction.source_id) :
+                rand(transaction.source_id + 1, id_base);
+
+        // can be made into a function OR atomic integer
+        transaction_id_mutex.lock();
+        transaction.id = transaction_id_base++;
+        transaction_id_mutex.unlock();
+
+        auto& source_account = _accounts.at(transaction.source_id);
+        const auto& source_balance = source_account.balance;
+        // skip accounts with a balance of 0
+        if (source_balance == 0)
+            continue;
+        // generate a random amount, POSSIBLY UNSAFE if balance is 0
+
+        transaction.amount = rand(1, source_balance + 1);
         
+        // subtract the amount from the source account
+        // TODO: make the subtraction thread-safe
+        source_account.balance -= transaction.amount;
+        printf("Transaction %d from %d to %d: %d\n", transaction.id, transaction.source_id, transaction.destination_id, transaction.amount);
+
+        // push the transaction to the list of transactions
+        // TODO: make the push thread-safe
+        transactions.push_front(transaction);
     }
+}
+
+void check_transactions_worker() {
+    // TODO
 }
 
 
@@ -115,6 +156,20 @@ int main(int argc, char** argv) {
     writer_thread.join();
     reader_thread.join();
 
+
+    // TODO: launch, from time to time, check transaction thread & join it
+
+    vector<std::thread> run_transactions_threads;
+
+    for (int i = 0; i < THREADS; ++i) {
+        run_transactions_threads.emplace_back(run_transactions_worker);
+    }
+
+    for (auto &runTransactionsThread: run_transactions_threads) {
+        runTransactionsThread.join();
+    }
+
+    print_all_accounts(_accounts);
 
     return 0;
 }
