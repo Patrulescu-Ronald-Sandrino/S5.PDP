61172d938a4549399255d85b576fa90c3e6ec43b
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-11-07 23:55:42 +0200

    add README.md add notes on how to do it

--- labs/homework3/main.cpp
+++ labs/homework3/main.cpp
@@ -2,10 +2,29 @@
 #include "Matrix.h"
 
 
+/*
+ class ThreadsApproach {
+     factorMatrix1
+     factorMatrix2
+     resultMatrix => get/set
+     numberOfThreads
+     generationStrategy
+}
+ */
+
 int main() {
     Matrix m = Matrix(2);
 
     cout << m;
+    /*
+    ThreadsApproach threadsApproach = ManualThreadsApproach(numberOfThreads, generationStrategy);
+    auto start = std::chrono::high_resolution_clock::now();
+    Matrix result = threadsApproach.run();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+    cout << result;
+     */
 
     return 0;
 }
