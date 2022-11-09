a313f5245085bc3c5d5459c09de5ebdbaf314c0c
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-11-09 19:35:47 +0200

    play around with values

--- labs/homework3/main.cpp
+++ labs/homework3/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <chrono>
-#include "domain/Matrix.h"
-#include "generation_strategy/GenerationStrategy.h"
 #include "threads_approach/ManualThreadsApproach.h"
+#include "threads_approach/ThreadPoolThreadsApproach.h"
+#include "generation_strategy/GenerationStrategy.h"
 #include "generation_strategy/RowStrategy.h"
 #include "generation_strategy/ColumnStrategy.h"
 #include "generation_strategy/KthStrategy.h"
@@ -10,19 +10,23 @@
 
 int main() {
     // --------------------- play with these values ---------------------
-    size_t matrixSize = 9;
-    int numberOfThreads = 3;
+    size_t matrixSize = 20;
+    int numberOfThreads = 20;
     GenerationStrategy* generationStrategy = new RowStrategy();
-    auto threadsApproachFactory = ManualThreadsApproachFactory();
+    auto threadsApproachFactory = ThreadPoolThreadsApproachFactory();
+    // 9 3 row manual => ~250μs
+    // 9 3 row threadpool => ~250μs
+    // 15 10 row manual => μs
+    // 15 10 row threadpool => μs
     // ------------------------------------------------------------------
 
     auto start = std::chrono::high_resolution_clock::now();
-    Matrix result = threadsApproachFactory.createThreadsApproach(matrixSize, numberOfThreads, *generationStrategy)->run();
+    auto result = threadsApproachFactory.createThreadsApproach(matrixSize, numberOfThreads, *generationStrategy)->run();
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
     cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
-    cout << result;
+//    cout << result;
 
     delete generationStrategy;
 
