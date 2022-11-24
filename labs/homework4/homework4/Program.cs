c6ee389ca39c1deb9d30c6db158d7b9a96c65c54
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-11-25 00:23:29 +0200

    [lab4] add implementation 3

--- labs/homework4/homework4/Program.cs
+++ labs/homework4/homework4/Program.cs
@@ -8,7 +8,8 @@
                 args = new[] { "www.google.com", "www.bing.com", "www.cs.ubbcluj.ro" };
 
             // Impl1.Run(args);
-            Impl2.Run(args);
+            // Impl2.Run(args);
+            Impl3.Run(args);
         }
     }
 }
