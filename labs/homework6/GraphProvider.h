6033dd4de72fe9f10e7c86efc5b20729b2a25562
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-12-23 21:05:44 +0200

    [lab6] loop only over the nodes

--- labs/homework6/GraphProvider.h
+++ labs/homework6/GraphProvider.h
@@ -21,7 +21,6 @@ public:
 
 Graph GraphProvider::withHamiltonianCycle = Graph::Factory::create({0, 1, 2, 3, 4}, {{1}, {2}, {4}, {0}, {3}}); // http://graphonline.ru/en/?graph=ZtbvgGfHcXvBmoXr
 Graph GraphProvider::withoutHamiltonianPath = Graph::Factory::create({0, 1, 2, 3, 4}, {{1}, {2}, {}, {0}, {3}}); // http://graphonline.ru/en/?graph=ZtbvgGfHcXvBmoXr
-//Graph GraphProvider::random = Graph::Factory::random(5, 5);
 
 
 #endif //HOMEWORK6_GRAPHPROVIDER_H
