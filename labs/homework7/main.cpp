ee2a4516fee332e2f2b946083a54d07bef6cec62
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-01-13 01:35:08 +0200

    update README.md

--- labs/homework7/main.cpp
+++ labs/homework7/main.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 int main() {
     MPI_Init(nullptr, nullptr);
+    int me;
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &me);
 
-    cout << "Hello, world!" << endl;
 
     MPI_Finalize();
     return 0;
